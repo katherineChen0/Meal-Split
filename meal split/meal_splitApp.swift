e82b8efd2a622e0db2920a8472ac49a738dce646
katherineChen0 <katherine05chen@gmail.com>
2024-12-16 12:56:47 -0800

    Add all files for the project

---
+++ meal split/meal_splitApp.swift
@@ -0,0 +1,17 @@
+//
+//  meal_splitApp.swift
+//  meal split
+//
+//  Created by Katherine on 11/22/24.
+//
+
+import SwiftUI
+
+@main
+struct meal_splitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
