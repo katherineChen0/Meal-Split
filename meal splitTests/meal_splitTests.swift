2246d85053f10d2e21e08823f539ce921b77a0cf
katherineChen0 <katherine05chen@gmail.com>
2024-12-16 12:38:10 -0800

    Initial Commit

---
+++ meal splitTests/meal_splitTests.swift
@@ -0,0 +1,17 @@
+//
+//  meal_splitTests.swift
+//  meal splitTests
+//
+//  Created by Katherine on 11/22/24.
+//
+
+import Testing
+@testable import meal_split
+
+struct meal_splitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
